05a8e380c443112d9922412c96fb5e7c487b270f
LENOVO <huyentrangnb20@gmail.com>
2020-12-25 09:27:54 +0700

    update10

--- app/src/main/java/com/e/doan3/lichsu.java
+++ app/src/main/java/com/e/doan3/lichsu.java
@@ -99,6 +99,7 @@ public class lichsu extends AppCompatActivity {
             }
         });
         al.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
